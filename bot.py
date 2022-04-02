b357009bbd5f3aea5011194f57c0d41f55ddf83c
GeorgeMoto <georgemotorin@gmail.com>
2022-04-03 01:06:56 +0800

    migration test pub -> priv

--- bot.py
+++ bot.py
@@ -1,12 +1,23 @@
 import telebot
 from Data import *
 from telebot import types
+from config import *
 
-bot = telebot.TeleBot("5263039063:AAEKR__jHHkhnlc5CmhcYR-q4LNJQXyUpEE")
+bot = telebot.TeleBot(BOT_TOKEN)
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
+    if not message.from_user.is_bot:
+
+        print(message.from_user)
+
+        users.update({message.from_user.id: {
+            'username': message.from_user.username,
+            'first_name': message.from_user.first_name,
+            'last_name': message.from_user.last_name}
+        })
+
     keyboard = types.InlineKeyboardMarkup(row_width=1)
 
     key_information = types.InlineKeyboardButton(text="🧐 Что это такое?", callback_data="key_information")
@@ -22,11 +33,17 @@ def start(message):
 @bot.message_handler(commands=["help"])
 def start(message):
     bot.send_message(message.chat.id, text=help_text, parse_mode="html")
+    print(users)
+
+
+# @bot.message_handler(commands=["push"])
+# def send_all(message):
+#     if message.from_user.id in admins:
+#         bot.send_message(message.chat.id, text="команда для админа", parse_mode="html")
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-
     if call.data == "key_information":
         bot.send_message(call.message.chat.id, information_text, parse_mode="html")
     elif call.data == "key_download":
---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "5263039063:AAEKR__jHHkhnlc5CmhcYR-q4LNJQXyUpEE"
+
+admins = (425895564,)
+
+PATH_TO_DB = "users_db.db"
